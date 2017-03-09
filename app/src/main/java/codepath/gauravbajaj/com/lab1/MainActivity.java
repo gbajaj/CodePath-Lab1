7b4d3fe588769638eac1754996bca6db4961b3b0
Bajaj, Gaurav <gauravb@telenav.com>
2017-03-08 19:27:36 -0800

    Login page implemented,Bingo!

--- app/src/main/java/codepath/gauravbajaj/com/lab1/MainActivity.java
+++ app/src/main/java/codepath/gauravbajaj/com/lab1/MainActivity.java
@@ -2,6 +2,10 @@ package codepath.gauravbajaj.com.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button cancel = (Button) findViewById(R.id.cancel);
+        final Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Logging In...", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Bbye ", Toast.LENGTH_LONG).show();
+                MainActivity.this.onBackPressed();
+            }
+        });
+
+
     }
+
 }
